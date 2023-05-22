932d0264a2a721b7b46029d579ae095d2ef172f6
elmarhoppieland <114440420+elmarhoppieland@users.noreply.github.com>
2023-05-22 19:11:05 +0200

    Added comments and removed obsolete code

--- Resources/Analyzer.gd
+++ Resources/Analyzer.gd
@@ -1,5 +1,7 @@
 extends Node
 
+## Singleton for functions regarding the Analyzer.
+
 # ==============================================================================
 ## The status of the data. To obtain the status, use [method get_data_status].
 enum DataStatus {
@@ -7,28 +9,35 @@ enum DataStatus {
 	NEW_DATA_FOUND, ## The data is from the current Analyzer version, but new log files were created since the last Analyzer launch.
 	OUTDATED_VERSION ## The data was from a different (usually older) Analyzer version. New data may or may not be found.
 }
+## The tabs in the Analyzer. To obtain a tab, use [method get_tab] or [code]TabName.get_tab()[/code].
 enum Tab {
-	HISTORY,
-	WINS,
-	STATISTICS,
-	TIMELINE,
-	ERRORS
+	HISTORY, ## The [History] tab.
+	WINS, ## The [Wins] tab.
+	STATISTICS, ## The [GlobalStatistics] tab.
+	TIMELINE, ## The [TimeLine] tab.
+	ERRORS ## The [Errors] tab.
 }
 # ==============================================================================
-const SAVE_DATA_DIRECTORY := "user://Profiles"
-const SAVE_DATA_DIRECTORY_DEBUG := "user://Profiles_debug"
-const SAVE_DATA_BACKUPS_DIRECTORY := "user://Backups"
-const SAVE_DATA_BACKUPS_DIRECTORY_DEBUG := "user://Backups_debug"
-const SAVE_DATA_FILENAME := "profiles%d.dcstat"
+const _SAVE_DATA_DIRECTORY := "user://Profiles"
+const _SAVE_DATA_DIRECTORY_DEBUG := "user://Profiles_debug"
+const _SAVE_DATA_FILENAME := "profiles%d.dcstat"
 
-## The path to the quests file while not in debug mode.
+## The path to the quests file.
 const QUESTS_FILE := "user://quests.dca"
 ## The path to the quests file while in debug mode.
 const QUESTS_FILE_DEBUG := "user://quests-debug.dca"
-## The path to the profiles file while not in debug mode.
+## The path to the backup of quests file.
+const QUESTS_BACKUP_FILE := "user://quests-backup.dca"
+## The path to the backup of quests file while in debug mode.
+const QUESTS_BACKUP_FILE_DEBUG := "user://quests-debug-backup.dca"
+## The path to the profiles file.
 const PROFILES_FILE := "user://profiles.dca"
 ## The path to the profiles file while in debug mode.
 const PROFILES_FILE_DEBUG := "user://profiles-debug.dca"
+## The path to the backup of the profiles file.
+const PROFILES_BACKUP_FILE := "user://profiles-backup.dca"
+## The path to the backup of the profiles file while in debug mode.
+const PROFILES_BACKUP_FILE_DEBUG := "user://profiles-debug-backup.dca"
 
 ## The path to the settings file.
 const SETTINGS_FILE := "user://settings.cfg"
@@ -99,29 +108,28 @@ func save_settings(new_settings: SettingsFile) -> void:
 	new_settings.save_settings()
 
 
-func get_backup_directory() -> String:
-	if OS.is_debug_build():
-		return SAVE_DATA_BACKUPS_DIRECTORY_DEBUG
-	
-	return SAVE_DATA_BACKUPS_DIRECTORY
-
-
+## Returns the path to the savedata directory.
+## [br][br][b]Note:[/b] This method is [b]deprecated[/b] as savedata files are no longer used.
 func get_savedata_directory() -> String:
 	if OS.is_debug_build():
-		return SAVE_DATA_DIRECTORY_DEBUG
+		return _SAVE_DATA_DIRECTORY_DEBUG
 	
-	return SAVE_DATA_DIRECTORY
+	return _SAVE_DATA_DIRECTORY
 
 
+## Returns the path to the savedata file with the given [code]index[/code].
+## [br][br][b]Note:[/b] This method is [b]deprecated[/b] as savedata files are no longer used.
 func get_savedata_path(index: int) -> String:
 	var dir_path := get_savedata_directory()
 	
 	if index < 0:
 		index += DirAccess.get_files_at(dir_path).size()
 	
-	return dir_path.path_join(SAVE_DATA_FILENAME % index)
+	return dir_path.path_join(_SAVE_DATA_FILENAME % index)
 
 
+## Opens the savedata file at the given [code]index[/code], using [constant FileAccess.WRITE].
+## [br][br][b]Note:[/b] This method is [b]deprecated[/b] as savedata files are no longer used.
 func open_savedata_file(index: int, allow_create: bool = false) -> FileAccess:
 	var path := get_savedata_path(index)
 	if allow_create and not FileAccess.file_exists(path):
@@ -135,10 +143,8 @@ func open_savedata_file(index: int, allow_create: bool = false) -> FileAccess:
 	return file
 
 
-func savedata_file_exists(index: int) -> bool:
-	return FileAccess.file_exists(get_savedata_path(index))
-
-
+## Returns the savedata in the file in the given [code]index[/code] as a [Dictionary].
+## [br][br][b]Note:[/b] This method is [b]deprecated[/b] as savedata files are no longer used.
 func get_savedata(index: int) -> Dictionary:
 	var file := open_savedata_file(index)
 	var json = JSON.parse_string(file.get_as_text())
@@ -146,6 +152,12 @@ func get_savedata(index: int) -> Dictionary:
 		return {}
 	
 	return json
+
+
+## Returns the path to the backup file of the quests file (i.e. [constant QUESTS_BACKUP_FILE] or
+## [constant QUESTS_BACKUP_FILE_DEBUG]).
+func get_quests_backup_file_path(debug: bool = OS.is_debug_build()) -> String:
+	return QUESTS_BACKUP_FILE_DEBUG if debug else QUESTS_BACKUP_FILE
 
 
 ## Returns the path to the quests file (i.e. [constant QUESTS_FILE] or
@@ -157,6 +169,12 @@ func get_quests_file_path(debug: bool = OS.is_debug_build()) -> String:
 ## Opens the quests file at path [constant QUESTS_FILE] and returns it.
 func open_quests_file(flags: FileAccess.ModeFlags = FileAccess.READ) -> FileAccess:
 	return FileAccess.open(get_quests_file_path(), flags)
+
+
+## Returns the path to the backup file of the profiles file (i.e. [constant PROFILES_BACKUP_FILE]
+## or [constant PROFILES_BACKUP_FILE_DEBUG]).
+func get_profiles_backup_file_path(debug: bool = OS.is_debug_build()) -> String:
+	return PROFILES_BACKUP_FILE_DEBUG if debug else PROFILES_BACKUP_FILE
 
 
 ## Returns the path to the profiles file (i.e. [constant PROFILES_FILE] or
--- Resources/ProfileLoader.gd
+++ Resources/ProfileLoader.gd
@@ -15,6 +15,8 @@ var _current_profile: Profile
 var errors := []
 # ==============================================================================
 
+## Forces an update of the data, essentially reparsing all the data. Any data
+## that is from log files that no longer exist will not be reparsed.
 func update_profiles() -> void:
 	if Analyzer.is_first_launch():
 		LoadingScreen.start(DemonCrawl.get_logs_count(), "Initializing...")
@@ -49,6 +51,9 @@ func update_profiles() -> void:
 	read_logs_dir(1)
 
 
+## Loads the profiles stored in the user's data directory, and parses new data
+## if able.
+## [br][br]To retrieve the profiles, use [method get_profiles] or [method get_used_profiles].
 func load_profiles(freeze_data: bool = false) -> void:
 	if Analyzer.is_first_launch():
 		LoadingScreen.start(DemonCrawl.get_logs_count(), "Initializing...")
@@ -89,10 +94,6 @@ func load_profiles(freeze_data: bool = false) -> void:
 	
 	LoadingScreen.set_step_count(DemonCrawl.get_logs_count() - read_index + 2)
 	
-#	LoadingScreen.set_message("Removing excess data files...")
-#	remove_excess_savedata_files()
-#	LoadingScreen.progress_increment()
-	
 	print_rich("[color=aqua]Save data is outdated.[/color]\n[color=aqua]Reading log files at index %s and beyond...[/color]" % read_index)
 	read_logs_dir(read_index)
 	
@@ -105,6 +106,8 @@ func load_profiles(freeze_data: bool = false) -> void:
 	# LoadingScreen should be finished automatically
 
 
+## Initializes the Analyzer, for the first launch (when the data files do not yet exist).
+## Parses data from the log files and stores it into the user's data directory.
 func initiate_first_launch() -> void:
 	print_rich("[color=green]First launch detected. Initializing...[/color]")
 	
@@ -117,62 +120,14 @@ func initiate_first_launch() -> void:
 	read_logs_dir(1)
 
 
-## Reads the saved data and sets the properties to the stored values.
-## [br][br]Returns the index of the log file that should be read. Returns [code]-1[/code]
-## if an error occurred. Returns [code]0[/code] if no log files should be read.
-## [br][br][b]Note:[/b] The returned index is 1-indexed, so that the index matches
-## with how DemonCrawl indexes its log files.
-func _read_saved_data() -> int:
-	match Analyzer.get_data_status():
-		Analyzer.DataStatus.UP_TO_DATE:
-			load_from_json(Analyzer.get_savedata(-1))
-			return 0
-	
-	create_backups()
-	
-	var file := Analyzer.open_savedata_file(-1)
-	if not file:
-		push_error("Error occurred during read operation: %s. Aborting process..." % error_string(FileAccess.get_open_error()))
-		return -1
-	
-	var text := file.get_as_text()
-	var parse = JSON.parse_string(text)
-	if parse is Dictionary:
-		if "version" in parse:
-			if parse.version == Analyzer.get_version():
-				# there is no update; we now want to load from this savedata file
-				# and start reading from the log file after the one the
-				# savedata file is associated with
-				load_from_json(parse)
-				
-				var log_index := get_log_file_index(parse.start_unix) + 1
-				
-				move_savedata_files(DemonCrawl.get_logs_count() - log_index)
-				
-				return log_index
-			
-			if parse.version < "1.3.0":
-				_update_1_2()
-		
-		# there is an update; we now want to load from the last file that
-		# does not have a log file associated with it
-		update_savedata()
-		
-		# we need to read from the first log file; the data that was saved
-		# should be overwritten with the data in the log files
-		return 1
-	
-	push_error("Invalid data in the last profiles.dcstat file. Aborting read operation...")
-	file.close()
-	return -1
-
-
 ## Reads the saved data and sets the singleton's properties to the stored values.
 ## [br][br]Returns the index of the log file that should be read. Returns [code]-1[/code]
 ## if an error occurred. Returns [code]0[/code] if no log files should be read.
 ## [br][br][b]Note:[/b] The returned index is 1-indexed, so that the index matches
 ## with how DemonCrawl indexes its log files.
 func read_saved_data() -> int:
+	create_backups()
+	
 	match Analyzer.get_data_status():
 		Analyzer.DataStatus.UP_TO_DATE:
 			read_quests()
@@ -230,6 +185,8 @@ func read_quests(condition: Callable = Callable()) -> void:
 		quests.append(quest)
 
 
+## Reads the profiles file and adds keys into the [member profiles] [Dictionary]
+## for each [Profile], and adds each [Quest] into the corresponding profile.
 func read_profiles() -> void:
 	var file := Analyzer.open_profiles_file()
 	var json_string := file.get_line()
@@ -252,63 +209,17 @@ func read_profiles() -> void:
 		profiles[profile_name] = profile
 
 
+## Reads the data stored in savedata files. This is used to update from savedata
+## formatting.
 func read_savedata_files(before_timestamp: String) -> void:
 	var index := get_savedata_index_before_unix(TimeHelper.get_unix_time_from_timestamp(before_timestamp))
 	load_from_savedata(Analyzer.get_savedata(index))
 
 
-## Copies the entire savedata directory into the backup directory.
+## Creates backups of the quests and profiles files.
 func create_backups() -> void:
-	if not DirAccess.dir_exists_absolute(Analyzer.get_backup_directory()):
-		DirAccess.make_dir_absolute(Analyzer.get_backup_directory())
-	for file in DirAccess.get_files_at(Analyzer.get_savedata_directory()):
-		DirAccess.copy_absolute(Analyzer.get_savedata_directory().path_join(file), Analyzer.get_backup_directory().path_join(file))
-
-
-## Returns the index of the log file that starts at unix [code]start_unix[/code],
-## or [code]0[/code] if there is no such file.
-## [br][br][b]Note:[/b] The log files's starting timestamp should match [b]exactly[/b]
-## with [code]start_unix[/code]. Even if they differ by 1 second, this method
-## will not find it.
-func get_log_file_index(start_unix: int) -> int:
-	for log_index in range(1, DemonCrawl.get_logs_count() + 1):
-		var log_file := DemonCrawl.open_log_file(log_index)
-		
-		var line := log_file.get_line()
-		var log_timestamp := line.get_slice("]", 0).trim_prefix("[")
-		var log_unix := TimeHelper.get_unix_time_from_timestamp(log_timestamp)
-		if start_unix == log_unix:
-			return log_index
-	
-	return 0
-
-
-## Reads from the first savedata file that no longer has a log file, and moves all
-## savedata files so that all data after that will be overwritten. This is typically
-## used when updating the Analyzer.
-func update_savedata() -> void:
-	print_rich("[color=green]Updating...[/color]")
-	
-	var first_log_file := DemonCrawl.open_log_file(1)
-	var first_log_unix := TimeHelper.get_unix_time_from_timestamp(first_log_file.get_line().get_slice("]", 0).trim_prefix("["))
-	
-	first_log_file.close()
-	
-	var savedata_index := get_savedata_index_before_unix(first_log_unix)
-	if savedata_index < 0:
-		# this shouldn't every happen but if it does then there's no reason to continue
-		return
-	
-	var savedata_file := Analyzer.open_savedata_file(savedata_index)
-	
-	var savedata_text := savedata_file.get_as_text()
-	var savedata_parse = JSON.parse_string(savedata_text)
-	if savedata_parse is Dictionary:
-		load_from_json(savedata_parse)
-	
-	savedata_file.close()
-	
-	save_data_to_disk()
+	DirAccess.copy_absolute(Analyzer.get_quests_file_path(), Analyzer.get_quests_backup_file_path())
+	DirAccess.copy_absolute(Analyzer.get_profiles_file_path(), Analyzer.get_profiles_backup_file_path())
 
 
 ## Returns the index of the last savedata file that started before [code]unix[/code]
@@ -340,43 +251,11 @@ func load_from_json(json: Dictionary) -> void:
 
 
 func load_from_savedata(json: Dictionary) -> void:
-	for property in json:
-		match property:
-			"profiles":
-				for profile in json.profiles:
-					profiles[profile] = HistoryData.from_json(json.profiles[profile], Profile)
-			"errors":
-				errors = json[property]
+	load_from_json(json)
 	
 	for profile in get_used_profiles():
 		quests.append_array(profile.quests)
 	sort_quests()
-
-
-## Moves all savedata files so that the file at index [code]new_zero_index[/code]
-## is now at index [code]0[/code], and all files after that are moved accordingly.
-func move_savedata_files(new_zero_index: int) -> void:
-	if new_zero_index < 1:
-		# we don't need to move any files
-		return
-	
-	for index in DemonCrawl.get_logs_count() - new_zero_index:
-		var current_path := Analyzer.get_savedata_path(new_zero_index + index)
-		var new_path := Analyzer.get_savedata_path(index)
-		var error := DirAccess.rename_absolute(current_path, new_path)
-		if error:
-			push_error("Error occurred during move operation: %s. Can't rename file '%s' to '%s'" % [error_string(error), Analyzer.get_savedata_path(new_zero_index + index), Analyzer.get_savedata_path(index)])
-			breakpoint
-
-
-func remove_excess_savedata_files() -> void:
-	var index := DemonCrawl.get_logs_count() + 1
-	while true:
-		var path := Analyzer.get_savedata_path(index)
-		if not FileAccess.file_exists(path):
-			return
-#		OS.move_to_trash(ProjectSettings.globalize_path(path))
-		DirAccess.remove_absolute(path)
 
 
 func read_logs_dir(starting_index: int) -> void:
@@ -384,7 +263,6 @@ func read_logs_dir(starting_index: int) -> void:
 		LoadingScreen.set_message("Parsing %s..." % (DemonCrawl.LOG_FILE_NAME % index))
 		read_log(index)
 		LoadingScreen.progress_increment()
-
 
 
 func save_data_to_disk() -> void:
@@ -395,15 +273,6 @@ func save_data_to_disk() -> void:
 		file.store_line(JSON.stringify(quest.to_json()))
 	file = Analyzer.open_profiles_file(FileAccess.WRITE)
 	file.store_line(JSON.stringify(get_profile_indexes()))
-
-
-func save_json(json: Dictionary, index: int) -> void:
-	var file := FileAccess.open(Analyzer.get_savedata_path(index), FileAccess.WRITE)
-	if not file:
-		push_error("Error occurred during write operation: %s. Aborting process..." % error_string(FileAccess.get_open_error()))
-		return
-	
-	file.store_line(JSON.stringify(json))
 
 
 func read_log(index: int) -> void:
@@ -502,30 +371,6 @@ func find_quests() -> Array[Quest]:
 
 # ------------------------------------------------------------------------------
 # _update() functions below
-
-func _update_1_2() -> void:
-	var quests_file := FileAccess.open("user://quests.dca", FileAccess.WRITE)
-	
-	quests.clear()
-	
-	var profile_indexes := {}
-	for profile in get_used_profiles():
-		quests.append_array(profile.quests)
-	quests.sort_custom(func(a: Quest, b: Quest):
-		var a_unix := TimeHelper.get_unix_time_from_timestamp(a.creation_timestamp)
-		var b_unix := TimeHelper.get_unix_time_from_timestamp(b.creation_timestamp)
-		return a_unix < b_unix
-	)
-	for profile in get_used_profiles():
-		profile_indexes[profile.name] = []
-		for quest in profile.quests:
-			profile_indexes[profile.name].append(quests.find(quest))
-	for quest in quests:
-		var line := JSON.stringify(quest.to_json())
-		quests_file.store_line(line)
-	var profiles_file := FileAccess.open("user://profiles.dca", FileAccess.WRITE)
-	profiles_file.store_string(JSON.stringify(profile_indexes))
-
 
 func _update_from_savedata() -> void:
 	LoadingScreen.set_message("Updating saved data...")
